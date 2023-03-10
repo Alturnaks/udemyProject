eeb2fa990ddbc71256b9dd13c46a1017a9661b7b
Kuanysh <kuanish2203@gmail.com>
2023-03-10 19:28:13 +0600

    len

--- viceversa/viceversa/view.py
+++ viceversa/viceversa/view.py
@@ -7,7 +7,11 @@ def feedback(request):
 def reverse(request):
     reversed_text=request.GET['usertext']
     reversed_text=reversed_text[::-1]
-    return render(request,'reverse.html',{'reverse_text':reversed_text})
+    len_text=len(reversed_text)
+    
+    data= {'reverse_text':reversed_text ,'len_text':len_text}
+
+    return render(request, 'reverse.html', data)
 
 
 
